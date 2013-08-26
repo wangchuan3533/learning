c42c289440b44ec2367c5532a2fb7a760e6d0907
wangchuan <wangchuan3533@gmail.com>
2013-08-27 04:53:52 +0800

    tcp server based on libevent

--- socket/event/server.php
+++ socket/event/server.php
@@ -1,46 +1,96 @@
 <?php
-$socket = stream_socket_server ('tcp://0.0.0.0:2000', $errno, $errstr);
-stream_set_blocking($socket, 0);
-$base = event_base_new();
-$event = event_new();
-event_set($event, $socket, EV_READ | EV_PERSIST, 'ev_accept', $base);
-event_base_set($event, $base);
-event_add($event);
-event_base_loop($base);
 
-$GLOBALS['connections'] = array();
-$GLOBALS['buffers'] = array();
+error_reporting(E_ALL);
+set_time_limit(0);
 
-function ev_accept($socket, $flag, $base) {
-    static $id = 0;
-    
-    $connection = stream_socket_accept($socket);
-    stream_set_blocking($connection, 0);
-    
-    $id += 1;
-    
-    $buffer = event_buffer_new($connection, 'ev_read', NULL, 'ev_error', $id);
-    event_buffer_base_set($buffer, $base);
-    event_buffer_timeout_set($buffer, 30, 30);
-    event_buffer_watermark_set($buffer, EV_READ, 0, 0xffffff);
-    event_buffer_priority_set($buffer, 10);
-    event_buffer_enable($buffer, EV_READ | EV_PERSIST);
-    
-    // we need to save both buffer and connection outside
-    $GLOBALS['connections'][$id] = $connection;
-    $GLOBALS['buffers'][$id] = $buffer;
-}
+class TcpServer {
 
-function ev_error($buffer, $error, $id) {
-    event_buffer_disable($GLOBALS['buffers'][$id], EV_READ | EV_WRITE);
-    event_buffer_free($GLOBALS['buffers'][$id]);
-    fclose($GLOBALS['connections'][$id]);
-    unset($GLOBALS['buffers'][$id], $GLOBALS['connections'][$id]);
-}
+    protected $address = '0.0.0.0';
+    protected $port = 10000;
+    protected $socket;
+    protected $base;
+    protected $buffers = array();
+    protected $clients = array();
+    protected $index = 0;
 
-function ev_read($buffer, $id) {
-    while ($read = event_buffer_read($buffer, 256)) {
-        var_dump($read);
+    public function __construct() {
+    }
+
+    public function init() {
+
+        $socket = stream_socket_server ("tcp://$this->address:$this->port", $errno, $errstr);
+        stream_set_blocking($socket, 0);
+        $base = event_base_new();
+
+        $this->socket = $socket;
+        $this->base = $base;
+
+    }
+
+    public function start() {
+
+        $socket = $this->socket;
+        $base = $this->base;
+
+        $event = event_new();
+        event_set($event, $socket, EV_READ | EV_PERSIST, array($this, 'ev_accept'), $base);
+        event_base_set($event, $base);
+        event_add($event);
+
+        event_base_loop($base);
+    }
+
+    protected function ev_accept($socket, $flag, $base) {
+
+        $socket = $this->socket;
+        $base = $this->base;
+        $index = &$this->index; 
+        
+        $connection = stream_socket_accept($socket);
+        stream_set_blocking($connection, 0);
+        
+        $buffer = event_buffer_new($connection, array($this, 'ev_read'), NULL, array($this, 'ev_error'), ++$index);
+        event_buffer_base_set($buffer, $base);
+        event_buffer_timeout_set($buffer, 300, 5);
+        event_buffer_watermark_set($buffer, EV_READ, 0, 0xffffff);
+        event_buffer_priority_set($buffer, 10);
+        event_buffer_enable($buffer, EV_READ | EV_PERSIST);
+        
+        // we need to save both buffer and connection outside
+        $this->clients[$index] = $connection;
+        $this->buffers[$index] = $buffer;
+        //var_dump($this->clients);
+    }
+
+    protected function ev_error($buffer, $error, $index) {
+        event_buffer_disable($this->buffers[$index], EV_READ | EV_WRITE);
+        event_buffer_free($this->buffers[$index]);
+        fclose($this->clients[$index]);
+        unset($this->clients[$index], $this->buffers[$index]);
+        var_dump($error);
+        //var_dump($this->clients);
+    }
+
+    protected function ev_read($buffer, $index) {
+        while ($read = event_buffer_read($buffer, 1024)) {
+            $data = 'it works';
+            stream_socket_sendto($this->clients[$index], $this->http_package($data));
+            /*
+            event_buffer_disable($this->buffers[$index], EV_READ | EV_WRITE);
+            event_buffer_free($this->buffers[$index]);
+            fclose($this->clients[$index]);
+            unset($this->clients[$index], $this->buffers[$index]);
+            return;
+            */
+        }
+    }
+
+    protected function http_package($data) {
+        $httpStr = "HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\nServer: Tencent.WeLife.Server\r\nConnection: Close\r\n\r\n" . $data;
+        return $httpStr;
     }
 }
-?>
+
+$server = new TcpServer();
+$server->init();
+$server->start();
