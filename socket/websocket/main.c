745e2015782c1b661a21bd3b4e5274f4406857bb
wangchuan <wangchuan3533@gmail.com>
2013-09-17 10:47:15 +0800

    update

--- socket/websocket/main.c
+++ socket/websocket/main.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include "http_parser.h"
 #include "sha1.h"
+
+#define BUF_SIZE 1024
 
 struct conn {
     char *url;
@@ -13,7 +17,21 @@ struct conn {
     char *Sec_WebSocket_Key;
     char *Sec_WebSocket_Protocol;
     char **current_field;
+    int completed;
 };
+
+const char *secret = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+const char *req = "GET /demo HTTP/1.1\r\n"
+    "Host: example.com\r\n"
+    "Connection: Upgrade\r\n"
+    "Upgrade: websocket\r\n"
+    "Origin: http://example.com\r\n"
+    "Sec-WebSocket-Protocol: sample\r\n"
+    "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n"
+    "Sec-WebSocket-Version: 13\r\n"
+    "\r\n";
+
 
 void print_conn(struct conn *conn)
 {
@@ -65,60 +83,127 @@ int header_value_cb(http_parser *p, const char *buf, size_t len)
     conn->current_field = 0;
     return 0;
 }
-
-int handshake(char *key)
+int message_complete_cb(http_parser *p, const char *buf, size_t len)
 {
-    printf("HTTP/1.1 101 Switching Protocols\r\n"
+    struct conn *conn = (struct conn *)p->data;
+    conn->completed = 1;
+}
+
+int handshake(struct conn *conn, char *dst)
+{
+
+    char tmp1[128], tmp2[128];
+    sprintf(tmp1, "%s%s", conn->Sec_WebSocket_Key, secret);
+    sha1(tmp2, tmp1, strlen(tmp1) << 3);
+    base64enc(tmp1, tmp2, 20);
+ 
+    sprintf(dst, "HTTP/1.1 101 Switching Protocols\r\n"
             "Upgrade: websocket\r\n"
             "Connection: Upgrade\r\n"
             "Sec-WebSocket-Accept: %s\r\n"
-            "\r\n", key);
-
+            "\r\n", tmp1);
+    return 0;
 }
+
+int http_response(struct conn *conn, char *dst)
+{
+    sprintf(dst, "HTTP/1.1 200 OK\r\n"
+            "Server: WebSocket\r\n"
+            "Content-Length: %d\r\n"
+            "Connection: close\r\n"
+            "Content-Type: text/html\r\n"
+            "\r\n%s", strlen("hello world"), "hello world");
+    return 0;
+}
+
 int main()
 {
+    char buf[BUF_SIZE];
+    int sock_s, sock_c;
+    struct sockaddr_in addr_s, addr_c;
+    unsigned int len;
+    int received;
+
     int i, n;
     struct conn conn;
-    const char *secret = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-    const char *req = "GET /demo HTTP/1.1\r\n"
-        "Host: example.com\r\n"
-        "Connection: Upgrade\r\n"
-        "Upgrade: websocket\r\n"
-        "Origin: http://example.com\r\n"
-        "Sec-WebSocket-Protocol: sample\r\n"
-        "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n"
-        "Sec-WebSocket-Version: 13\r\n"
-        "\r\n";
 
-    char msg[128];
-    char dst[20];
-
-
+    /* http_parser */
     http_parser_settings settings;
     http_parser *parser;
+
+
+    /* Create the TCP socket */
+    if ((sock_s = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Failed to create socket\n");
+        exit(1);
+    }
+
+    /* Construct the server sockaddr_in structure */
+    memset(&addr_s, 0, sizeof(addr_s));
+    addr_s.sin_family = AF_INET;
+    addr_s.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr_s.sin_port = htons(9555);
+
+    /* Bind the server socket */
+    if (bind(sock_s, (struct sockaddr *) &addr_s, sizeof(addr_s)) < 0) {
+        perror("Failed to bind the server socket\n");
+        exit(1);
+    }
+
+    /* Listen on the server socket */
+    if (listen(sock_s, 128) < 0) {
+        perror("Failed to linten on the server socket\n");
+        exit(1);
+    }
+
 
     memset(&settings, 0, sizeof(settings));
     settings.on_url = request_url_cb;
     settings.on_header_field = header_field_cb;
     settings.on_header_value = header_value_cb;
+    settings.on_message_complete = message_complete_cb;
 
-    memset(&conn, 0, sizeof(conn));
 
     parser = malloc(sizeof(http_parser));
-    http_parser_init(parser, HTTP_REQUEST);
-    parser->data = &conn;
-    
-    n = http_parser_execute(parser, &settings, req, strlen(req));
-    if (parser->upgrade) {
-        printf("upgrade\n");
-    } else if (n != strlen(req)) {
-        printf("execute error\n");
+ 
+    /* Accept connections */
+    while (1) {
+        if ((sock_c = accept(sock_s, (struct sockaddr *) &addr_c, &len)) < 0) {
+            perror("Failed to accept the connections\n");
+            exit(1);
+        }
+
+        /* Receive messages */
+        if ((received = recv(sock_c, buf, BUF_SIZE, 0)) < 0) {
+            perror("Failed to receive message from client\n");
+            exit(1);
+        }
+
+        buf[received] = 0;
+        printf("receieved:%s\n", buf);
+        /* handshake */
+
+        http_parser_init(parser, HTTP_REQUEST);
+        parser->data = &conn;
+        memset(&conn, 0, sizeof(conn));
+        parser->data = &conn;
+        n = http_parser_execute(parser, &settings, buf, received);
+
+        if (parser->upgrade) {
+            handshake(&conn, buf);
+        } else if (n == received) {
+            http_response(&conn, buf);
+        } else {
+            http_response(&conn, buf);
+            printf("n=%d,  received=%d\n", n, received);
+        }
+
+        if (send(sock_c, buf, strlen(buf), 0) != strlen(buf)) {
+            perror("Failed to send messages to client\n");
+            exit(1);
+        }
+        close(sock_c);
     }
-    /*print_conn(&conn);*/
-
-    sprintf(msg, "%s%s", conn.Sec_WebSocket_Key, secret);
-    sha1(dst, msg, strlen(msg) << 3);
-    base64enc(msg, dst, 20);
-    handshake(msg);
+    close(sock_s);
+    exit(0);
 }
-
