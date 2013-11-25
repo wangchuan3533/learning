913d3ce7c7a420219dbc1d5ce158917ef4fb7bd7
wangchuan <wangchuan3533@gmail.com>
2013-11-25 12:55:33 +0000

    修改测试

--- php/lfsr/lfsr_test.php
+++ php/lfsr/lfsr_test.php
@@ -8,9 +8,9 @@ if (count($argv) > 1 && $argv[1] == 'init') {
 
 //$next = LFSR_Memcache::lfsr_memcache_get_next(1234);
 for ($i = 0; $i < 10; $i++) {
-    $next = LFSR_Memcache::lfsr_memcache_get(1234, rand(1, 100000));
+    $next = LFSR_Memcache::lfsr_memcache_get(1234, 10000);
     foreach ($next as $v) {
         echo "$v\n";
     }
-    sleep(rand(1,2));
+    usleep(10);
 }
