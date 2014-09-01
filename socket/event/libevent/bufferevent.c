5591df3965a6517d864d8f2948892b0cada86a89
wangchuan <wangchuan3533@gmail.com>
2014-09-01 17:05:28 +0800

    echo

--- socket/event/libevent/bufferevent.c
+++ socket/event/libevent/bufferevent.c
@@ -9,65 +9,28 @@
 #include <event2/buffer.h>
 #include <event2/bufferevent.h>
 
-#include <assert.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
 
-#define MAX_LINE 16384
-
-void do_read(evutil_socket_t fd, short events, void *arg);
-void do_write(evutil_socket_t fd, short events, void *arg);
-
-char
-rot13_char(char c)
-{
-    /* We don't want to use isalpha here; setting the locale would change
-     * which characters are considered alphabetical. */
-    if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
-        return c + 13;
-    else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
-        return c - 13;
-    else
-        return c;
-}
-
-void
-readcb(struct bufferevent *bev, void *ctx)
+void readcb(struct bufferevent *bev, void *ctx)
 {
     struct evbuffer *input, *output;
-    char *line;
+    char buf[1024];
     size_t n;
     int i;
     input = bufferevent_get_input(bev);
     output = bufferevent_get_output(bev);
 
-    while ((line = evbuffer_readln(input, &n, EVBUFFER_EOL_LF))) {
-        for (i = 0; i < n; ++i)
-            line[i] = rot13_char(line[i]);
-        evbuffer_add(output, line, n);
-        evbuffer_add(output, "\n", 1);
-        free(line);
-    }
-
-    if (evbuffer_get_length(input) >= MAX_LINE) {
-        /* Too long; just process what there is and go on so that the buffer
-         * doesn't grow infinitely long. */
-        char buf[1024];
-        while (evbuffer_get_length(input)) {
-            int n = evbuffer_remove(input, buf, sizeof(buf));
-            for (i = 0; i < n; ++i)
-                buf[i] = rot13_char(buf[i]);
-            evbuffer_add(output, buf, n);
-        }
-        evbuffer_add(output, "\n", 1);
+    while (evbuffer_get_length(input)) {
+        int n = evbuffer_remove(input, buf, sizeof(buf));
+        evbuffer_add(output, buf, n);
     }
 }
 
-void
-errorcb(struct bufferevent *bev, short error, void *ctx)
+void errorcb(struct bufferevent *bev, short error, void *ctx)
 {
     if (error & BEV_EVENT_EOF) {
         /* connection has been closed, do any clean up here */
@@ -82,8 +45,7 @@ errorcb(struct bufferevent *bev, short error, void *ctx)
     bufferevent_free(bev);
 }
 
-void
-do_accept(evutil_socket_t listener, short event, void *arg)
+void do_accept(evutil_socket_t listener, short event, void *arg)
 {
     struct event_base *base = arg;
     struct sockaddr_storage ss;
@@ -98,13 +60,11 @@ do_accept(evutil_socket_t listener, short event, void *arg)
         evutil_make_socket_nonblocking(fd);
         bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
         bufferevent_setcb(bev, readcb, NULL, errorcb, NULL);
-        bufferevent_setwatermark(bev, EV_READ, 0, MAX_LINE);
         bufferevent_enable(bev, EV_READ|EV_WRITE);
     }
 }
 
-void
-run(void)
+void run(void)
 {
     evutil_socket_t listener;
     struct sockaddr_in sin;
@@ -121,13 +81,6 @@ run(void)
 
     listener = socket(AF_INET, SOCK_STREAM, 0);
     evutil_make_socket_nonblocking(listener);
-
-#ifndef WIN32
-    {
-        int one = 1;
-        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
-    }
-#endif
 
     if (bind(listener, (struct sockaddr*)&sin, sizeof(sin)) < 0) {
         perror("bind");
@@ -146,11 +99,8 @@ run(void)
     event_base_dispatch(base);
 }
 
-int
-main(int c, char **v)
+int main(int c, char **v)
 {
-    setvbuf(stdout, NULL, _IONBF, 0);
-
     run();
     return 0;
 }
